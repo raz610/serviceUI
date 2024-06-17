c6539cda9eb6d5b5fbe07d06fd2590b968f7fd40
raz610 <151939455+raz610@users.noreply.github.com>
2024-06-17 22:24:25 +0300

    Add files via upload

---
+++ install.ps1
@@ -0,0 +1,8 @@
+﻿#.exe file source
+$FileLocation = "C:\temp\Hostname_UI_Icon\'שם המחשב שלי'.exe"
+
+#.exe file destination
+$DestPath =  "C:\Users\Public\Desktop"
+
+#copy .exe file from source to destination
+powershell.exe copy-item $FileLocation -destination $DestPath -Force
